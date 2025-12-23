29aef8bfef7b1093aff6d5c57094ebbd48082963
MubashirKhan001 <mk765pc@gmail.com>
2025-12-23 17:03:25 +0500

    counter-app-complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CounterApp from "./CounterApp"
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-120">
+      <h1 className="text-white font-bold text-2xl">REACT-COUNTER-APP!</h1>
+      <br />
+      <CounterApp />
+    </div>
+  )
+}
+
+export default App
---
+++ src/CounterApp.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+function CounterApp() {
+    const [counter, setCounter] = useState(0);
+
+    const counterIncrease = () => {
+        setCounter(counter + 1)
+    };
+    const counterDecrease = () => {
+         setCounter(counter - 1)
+    };
+
+    return (
+        <div className="flex flex-col justify-center items-center gap-y-6">
+            <div className="bg-white min-w-55 min-h-20 items-center flex justify-center text-3xl rounded text-red-500 font-bold">{counter}</div>
+            <div className="flex gap-x-2">
+                <button onClick={counterIncrease} className="cursor-pointer bg-white text-red-500  py-2 px-3 font-bold rounded">Increment</button>
+                <button onClick={counterDecrease} disabled={counter === 0} className="cursor-pointer bg-white text-red-500 px-3 py-2 font-bold rounded">Decrement</button>
+            </div>
+        </div>
+    )
+
+};
+export default CounterApp;
